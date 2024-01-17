c27bd4a80aefc7f285ddbfb385e11d7ab61874dc
Liliana Del Carmen Chora Monar <116900601+lilichora@users.noreply.github.com>
2024-01-16 19:14:39 -0500

    acabado

---
+++ vue-two-way-binding/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <TwoWayBinding />
+  </div>
+</template>
+
+<script>
+import TwoWayBinding from '@/components/TwoWayBinding.vue';
+export default {
+  name: 'App',
+  components: {
+    TwoWayBinding
+  }
+};
+</script>
+
+<style>
+/* Estilos globales de la aplicación */
+</style>
---
+++ vue-two-way-binding/src/components/TwoWayBinding.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div style="background-color: aquamarine;">
+    <body>
+      <p style="color: blue;">Ingresar nombre</p>
+      <input type="text" v-model="message">
+      <p >Este Trabajo fue realizado por el grupo 6 y sus Integrantes son :</p><p style="color: red;"> {{ message }}</p><p> Y nosotros aprendimos la funcion TwoWayBinding</p>
+    </body>
+  </div>
+</template>
+
+<script >
+export default {
+  data() {
+    return {
+      message: 'Liliana Chora, Ivonne Jacome, Douglas Moreno '
+    };
+  }
+};
+</script>
